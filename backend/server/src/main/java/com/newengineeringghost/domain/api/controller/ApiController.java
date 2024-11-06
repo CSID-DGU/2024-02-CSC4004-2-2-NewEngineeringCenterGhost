9a2d5f294297a5adadacc7753a05af920c45a835
hwnnn <2020112023@dgu.ac.kr>
2024-11-06 16:20:13 +0900

    chore: server 구조 생성

---
+++ backend/server/src/main/java/com/newengineeringghost/domain/api/controller/ApiController.java
@@ -0,0 +1,14 @@
+package com.newengineeringghost.domain.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/service")
+public class ApiController {
+    // Todo 빠른 측정
+
+    // Todo 정밀 측정
+
+    // Todo 사용자 정의 측정
+}
---
+++ backend/server/src/main/java/com/newengineeringghost/domain/api/service/ApiService.java
@@ -0,0 +1,18 @@
+package com.newengineeringghost.domain.api.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ApiService {
+
+    // Todo 빠른 측정
+
+    // Todo 정밀 측정
+
+    // Todo 사용자 정의 측정
+
+    // Todo 낚시성 정보 판별 기능
+
+    // Todo 링크 분석 및 데이터 처리
+
+}
