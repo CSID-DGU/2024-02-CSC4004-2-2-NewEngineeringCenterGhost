17ba01e84b9217855399d5c34a91c9e3c69d2519
dongju <smturtle2@kakao.com>
2024-11-08 14:23:45 +0900

    fix: 데이터 저장 파일 이름 형식 수정

--- train/preprocess.py
+++ train/preprocess.py
@@ -22,9 +22,9 @@ for i in range(0, len(dataset), 8192):
         x = m.morphs(x)
         x = np.array([model.wv[decomposeHangulText(word)] for word in x], dtype=np.float16)
         X[str(j)] = x
-    np.savez_compressed(f"train/data/dataset/dataset_{i//8192:04d}_X.npz", **X)
+    np.savez_compressed(f"train/data/dataset/dataset_{i//8192:02d}_X.npz", **X)
     
     y = ~dataset['label'][i:i+8192].values.astype(np.bool_)
-    np.save(f"train/data/dataset/dataset_{i//8192:04d}_y.npy", y)
+    np.save(f"train/data/dataset/dataset_{i//8192:02d}_y.npy", y)
 
 print("\nDone!")
