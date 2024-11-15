c25000dfafaf477396cc6730a59e24a2822550db
dongju <smturtle2@kakao.com>
2024-11-15 18:15:22 +0900

    feat: 에포크 실행을 위한 run_epoch 함수 추가 및 코드 정리

--- train/train.py
+++ train/train.py
@@ -19,17 +19,21 @@ metrics = Metrics()
 optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4)
 scaler = GradScaler()
 
-best_f1 = 0
-for epoch in range(epochs):
-    print(f'Epoch {epoch + 1}/{epochs}')
-    train_loss = 0
+def run_epoch(loader : DatasetLoader, train=False) -> tuple: # return loss, f1, acc
+    loss = 0
     total_batch = 0
     datasets = []
-    train_loader.shuffle()
+    loader.shuffle()
     metrics.reset()
-    for i in range(len(train_loader)):
-        datasets.append(train_loader[i])
-        if len(datasets) < load_size and i < len(train_loader) - 1:
+
+    if train:
+        model.train()
+    else:
+        model.eval()
+    
+    for i in range(len(loader)):
+        datasets.append(loader[i])
+        if len(datasets) < load_size and i < len(loader) - 1:
             continue
 
         dataset = ConcatDataset(datasets)
@@ -50,66 +54,41 @@ for epoch in range(epochs):
                     pred = model(x, padding_mask)
                     loss = criterion(pred, y) / update_size
                 scaler.scale(loss).backward()
-                train_loss += loss.item() * update_size
+                loss += loss.item() * update_size
                 metrics.update(pred > 0, y) 
-
+            
                 f1 = metrics.get('f1')
                 acc = metrics.get('acc')
-                printBar(i + 1, len(train_loader), '\rTotal Dataset:')
-                printBar(k + j - len(loaded) + 2, len(dataloader), 'Training:', f'{total_batch:6d} batches done | loss: {train_loss / total_batch:.4f} | F1: {f1:.4f} | Acc: {acc:.4f}')
-
+                printBar(i + 1, len(loader), '\rTotal Dataset:')
+                printBar(k + j - len(loaded) + 2, len(dataloader), 'Training:' if train else 'Validation:', f'{total_batch:6d} batches done | loss: {loss / total_batch:.4f} | F1: {f1:.4f} | Acc: {acc:.4f}')
+            
             loaded = []
             torch.cuda.empty_cache()
+
+            if not train:
+                continue
 
             scaler.step(optimizer)
             scaler.update()
             optimizer.zero_grad()
-        dataloader = None
-        dataset = None
-    print()
-
-    val_loss = 0
-    total_batch = 0
-    datasets = []
-    val_loader.shuffle()
-    metrics.reset()
-    for i in range(len(val_loader)):
-        datasets.append(val_loader[i])
-        if len(datasets) < load_size and i < len(val_loader) - 1:
-            continue
-
-        dataset = ConcatDataset(datasets)
-        datasets = []
-        dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-
-        loaded = []
-        for j, (x, padding_mask, y) in enumerate(dataloader):
-            loaded.append((x.to(device), padding_mask.to(device), y.to(device).unsqueeze(-1)))
-
-            if len(loaded) < update_size and j < len(dataloader) - 1:
-                continue
-            
-            torch.cuda.synchronize()
-            for k, (x, padding_mask, y) in enumerate(loaded):
-                total_batch += 1
-                with autocast('cuda', torch.bfloat16):
-                    pred = model(x, padding_mask)
-                    loss = criterion(pred, y) / update_size
-                val_loss += loss.item() * update_size
-                metrics.update(pred > 0, y) 
-
-                f1 = metrics.get('f1')
-                acc = metrics.get('acc')
-                printBar(i + 1, len(val_loader), '\rTotal Dataset:')
-                printBar(k + j - len(loaded) + 2, len(dataloader), 'Validation:', f'{total_batch:6d} batches done | loss: {val_loss / total_batch:.4f} | F1: {f1:.4f} | Acc: {acc:.4f}')
-
-            loaded = []
-            torch.cuda.empty_cache()
 
         dataloader = None
         dataset = None
     print()
-    if f1 > best_f1:
-        best_f1 = f1
+    return loss / total_batch, metrics.get('f1'), metrics.get('acc')
+
+best_f1 = 0
+for epoch in range(epochs):
+    print(f'Epoch {epoch + 1}/{epochs}')
+    train_loss, train_f1, train_acc = run_epoch(train_loader, train=True)
+    val_loss, val_f1, val_acc = run_epoch(val_loader)
+    print(f'Training: loss={train_loss:.4f}, f1={train_f1:.4f}, acc={train_acc:.4f} | Validation: loss={val_loss:.4f}, f1={val_f1:.4f}, acc={val_acc:.4f}')
+    
+    if val_f1 > best_f1:
+        best_f1 = val_f1
         torch.save(model.state_dict(), 'train/model.pth')
-        print(f'Saved model with F1: {best_f1:.4f}')
+        print('Model saved!')
+
+model.load_state_dict(torch.load('train/model.pth'))
+test_loss, test_f1, test_acc = run_epoch(test_loader)
+print(f'Test: loss={test_loss:.4f}, f1={test_f1:.4f}, acc={test_acc:.4f}')
