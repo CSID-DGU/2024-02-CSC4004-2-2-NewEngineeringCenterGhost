1a7d38c92cda19276137e18a90b8ad1104342948
chosungwon <chosung01@naver.com>
2024-11-18 19:51:42 +0900

    test: TestOCR.py url 파라미터 받아오기 구현

--- backend/modules/ocr/TestOCR.py
+++ backend/modules/ocr/TestOCR.py
@@ -1,10 +1,20 @@
-from PIL import Image
+
+import time
+
+#start = time.time()
+
+import urllib.request
 import pytesseract
 import cv2
+import sys
 
 #이미지 주소
-image_path = "/home/chosungwon/Downloads/test_card_news.png"
+image_path = []
 
+for i in range(1, len(sys.argv)):
+    image_path.append(sys.argv[i])
+
+"""
 #pillow lib 사용한 ocr
 image_PIL = Image.open(image_path)
 
@@ -12,13 +22,22 @@ image_PIL = image_PIL.convert("L")
 image_PIL = image_PIL.point(lambda x: 0 if x < 128 else 255)
 
 text_PIL = pytesseract.image_to_string(image_PIL, lang="kor+eng")
+"""
+
+#request를 이용한 이미지 받기
+for i in range(len(image_path)):
+    urllib.request.urlretrieve(image_path[i], f'backend/modules/ocr/TempImage/Temp{i:02d}.png')
 
 #opencv lib 사용한 ocr
-image_cv2 = cv2.imread(image_path)
+for i in range(len(image_path)):
+    image_cv2 = cv2.imread(f'backend/modules/ocr/TempImage/Temp{i:02d}.png')
 
-text_cv2 = pytesseract.image_to_string(image_cv2, lang="kor+eng")
+    text_cv2 = pytesseract.image_to_string(image_cv2, lang="kor+eng")
+
+    #print("추출된 텍스트(PIL): ", text_PIL)
+    print("추출된 텍스트(cv2): ", text_cv2)
 
 cv2.waitKey(0)
 
-print("추출된 텍스트(PIL): ", text_PIL)
-print("추출된 텍스트(cv2): ", text_cv2)
+#print(time.time() - start)
+#print(1.172527551651001 - 0.7256977558135986)
