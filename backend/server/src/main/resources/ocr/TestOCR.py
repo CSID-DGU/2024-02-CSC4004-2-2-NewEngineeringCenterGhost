6a22456cbc23a914aa3f66f1fdc5a76c056fda5d
dongju <smturtle2@kakao.com>
2024-12-02 22:21:18 +0900

    fix: TempImage 경로를 상대 경로로 수정

--- backend/server/src/main/resources/ocr/TestOCR.py
+++ backend/server/src/main/resources/ocr/TestOCR.py
@@ -11,18 +11,18 @@ for i in range(1, len(sys.argv)):
     image_path.append(sys.argv[i])
 
 #os lib를 사용한 TempImage 폴더 생성
-temp_path = f'backend/modules/ocr/TempImage'
+temp_path = f'./TempImage'
 
 if not os.path.exists(temp_path):
     os.makedirs(temp_path)
 
 #request를 이용한 이미지 받기
 for i in range(len(image_path)):
-    urllib.request.urlretrieve(image_path[i], f'backend/modules/ocr/TempImage/Temp{i:02d}.png')
+    urllib.request.urlretrieve(image_path[i], f'./TempImage/Temp{i:02d}.png')
 
 #opencv lib 사용한 ocr
 for i in range(len(image_path)):
-    image_cv2 = cv2.imread(f'backend/modules/ocr/TempImage/Temp{i:02d}.png')
+    image_cv2 = cv2.imread(f'./TempImage/Temp{i:02d}.png')
 
     text_cv2 = pytesseract.image_to_string(image_cv2, lang="kor+eng")
 
@@ -32,6 +32,6 @@ for i in range(len(image_path)):
 #os lib를 이용한 TempImage에 생성된 png 파일 삭제
 if os.path.exists(temp_path):
     for path in os.listdir(temp_path):
-        os.remove('backend/modules/ocr/TempImage/' + path)
+        os.remove('./TempImage/' + path)
 
 cv2.waitKey(0)
