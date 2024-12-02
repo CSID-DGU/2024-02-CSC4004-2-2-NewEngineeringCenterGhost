2d0d7b010859ccf0f0e3469a9f4379213a16675c
dongju <smturtle2@kakao.com>
2024-12-03 01:20:13 +0900

    fix: 낚시성 정보 해설 AI의 메시지 내용 수정

--- backend/server/src/main/resources/openai/TestOpenAi.py
+++ backend/server/src/main/resources/openai/TestOpenAi.py
@@ -10,7 +10,7 @@ openai.api_key = open(os.path.dirname(os.path.realpath(__file__)) + "/api.txt", 
 model = "gpt-4o-mini"
 
 messages = [
-    {"role":"system","content": "당신은 낚시성 정보로 판단된 기사에 대해 해설하는 AI입니다."},
+    {"role":"system","content": "당신은 낚시성 정보로 판단된 글에 대해 해설하는 AI입니다."},
     {"role":"system","content": "다음은 본문 내용입니다:"},
     {"role":"system","content": content},
     {"role":"system","content": "다음은 낚시성 정보라고 판단하는 데 중요한 역할을 한 문장들입니다:"},
